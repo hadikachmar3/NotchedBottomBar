981b7bef087cfeb0b63c6209ca08fce89c130c36
Hadi Kachmar <HADIKACHMAR8@GMAIL.COM>
2020-12-21 05:01:19 +0200

    notched bottom bar

---
+++ lib/screens/user_info.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UserInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+       child: Text('User info')
+      ),
+    );
+  }
+}
